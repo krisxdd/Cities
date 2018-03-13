7d38403f37dd8e507477e9323846ef34cf62ed87
Student <Student@193.107.34.198>
2018-03-13 11:17:51 +0100

    koszyk itp

---
+++ Cities2018/src/cities2018/Countries.java
@@ -0,0 +1,33 @@
+
+package cities2018;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Countries 
+{
+    private Map<String, String> countries=new HashMap();
+
+    public Countries() 
+    {
+        countries.put("Poland", "Warsaw");
+        countries.put("Germany", "Berlin");
+        countries.put("Russia", "Moscow");
+        countries.put("Great Britain", "London");
+        countries.put("France", "Paris");
+        countries.put("Spain", "Madrid");
+    }
+    @Override
+    public String toString()
+    {
+        return countries.toString();
+    }
+    public static void main(String[] args) 
+    {
+        Countries c=new Countries();
+        System.out.println(c);
+    }
+    
+    
+}
